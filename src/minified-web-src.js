f331cea8e88cb04a794930f65d2fb7733183fa50
tim <tim@tjansen.de>
2013-08-23 23:43:29 +0200

    Improving loop doc. Replacing null->_null, true->_true ... (shrinking size with newer closure versions)

--- src/minified-web-src.js
+++ src/minified-web-src.js
@@ -91,6 +91,8 @@ define('minified', function() {
 	
 	//// GLOBAL VARIABLES ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+	var _null = null, _true = true, _false = false;
+	
 	///#snippet webVars
 	/**
 	 * @const
@@ -237,10 +239,9 @@ define('minified', function() {
 	
 	//// GLOBAL FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	
 	/** @param s {?} */
 	function toString(s) { // wrapper for Closure optimization
-		return s!=null ? ''+s : '';
+		return s!=_null ? ''+s : '';
 	}
 	/**
 	 * @param s {?}
@@ -263,7 +264,7 @@ define('minified', function() {
 		return isType(n, 'number');
 	}
 	function isList(v) {
-		return v && v.length != null && !isString(v) && !isNode(v) && !isFunction(v);
+		return v && v.length != _null && !isString(v) && !isNode(v) && !isFunction(v);
 	}
 	function nonOp(v) {
 		return v;
@@ -297,7 +298,7 @@ define('minified', function() {
 		if (isList(list))
 			for (var i = 0; i < list.length; i++)
 				cb(list[i], i);
-		else if (list != null)
+		else if (list != _null)
 			cb(list, 0);
 		return list;
 	}
@@ -309,7 +310,7 @@ define('minified', function() {
 		return isType(f, 'function') && !f['item']; // item check as work-around webkit bug 14547
 	}
 	
-	function returnTrue() { return 1;}
+	function returnTrue() { return _true;}
 	
 	function wordRegExpTester(name, prop) {
 		var re = RegExp('\\b' + name + '\\b', 'i');
@@ -348,7 +349,7 @@ define('minified', function() {
 			flexiEach(func(value), function(node) {
 				if (isNode(node) &&!nodeIds[currentNodeId = getNodeId(node)]) {
 					result.push(node);
-					nodeIds[currentNodeId] = true;
+					nodeIds[currentNodeId] = _true;
 				}
 			});
 		});
@@ -357,10 +358,10 @@ define('minified', function() {
 	
 	// finds out the 'natural' height of the first element, the one if $$slide=1
 	function getNaturalHeight(elementList) {
-		var q = {'$position': 'absolute', '$visibility': 'hidden', '$display': 'block', '$height': null};
+		var q = {'$position': 'absolute', '$visibility': 'hidden', '$display': 'block', '$height': _null};
 		var oldStyles = elementList['get'](q);
 		elementList['set'](q);
-		var h = elementList['get']('$height', true);
+		var h = elementList['get']('$height', _true);
 		elementList['set'](oldStyles);
 		return h;
 	}
@@ -381,8 +382,8 @@ define('minified', function() {
 						e['stopPropagation']();
 					// @condblock ie8compatibility 
 					}
-					e.returnValue = false; // cancel for IE
-					e.cancelBubble = true; // cancel bubble for IE
+					e.returnValue = _false; // cancel for IE
+					e.cancelBubble = _true; // cancel bubble for IE
 					// @condend
 				}
 				// @cond debug } catch (ex) { error("Error in event handler \""+name+"\": "+ex); }
@@ -407,7 +408,7 @@ define('minified', function() {
     // for ready()
     function triggerDomReady() {
 		flexiEach(DOMREADY_HANDLER, callArg);
-		DOMREADY_HANDLER = null;
+		DOMREADY_HANDLER = _null;
     }
     
     function ready(handler) {
@@ -429,10 +430,10 @@ define('minified', function() {
 		return function() {
 			var list = $(_document.createElement(elementName));
 			// TODO: attributes!=null only needed with UTIL. Web's isObject is simpler.
-			(isList(attributes) || (attributes != null && !isObject(attributes)) ) ? list['add'](attributes) : list['set'](attributes)['add'](children);
+			(isList(attributes) || (attributes != _null && !isObject(attributes)) ) ? list['add'](attributes) : list['set'](attributes)['add'](children);
 			if (onCreate)
 				onCreate(list);
-			return list; 
+			return list;
 		};
 	}
 
@@ -485,9 +486,9 @@ define('minified', function() {
 					var a = node;
 					while (a = a['parentNode']) {
 						if (a === parent)
-							return true;
+							return _true;
 						if (childOnly)
-							return false;
+							return _false;
 					}
 					// fall through to return undef
 				});
@@ -553,12 +554,12 @@ define('minified', function() {
 		else {
 			if (context) { 
 				return function(v) { 
-					return $(selector, context)['find'](v)!=null; 
+					return $(selector, context)['find'](v)!=_null; 
 				};
 			}
 			else {
 				$(selector)['each'](function(node) {
-					nodeSet[getNodeId(node)] = true;
+					nodeSet[getNodeId(node)] = _true;
 				});
 				return function(v) { 
 					return nodeSet[getNodeId(v)]; 
@@ -578,7 +579,7 @@ define('minified', function() {
  		var deferred = [];   // functions to call when set() is invoked
  	 	
     	var set = function (newState, newValues) {
-    		if (state == null) {
+    		if (state == _null) {
 	    		state = newState;
 	    		values = newValues;
    				delay(function() {
@@ -655,20 +656,20 @@ define('minified', function() {
 	    		try {
 	    			var f = (state ? onFulfilled : onRejected);
 	    			if (isFunction(f)) {
-		   				var r = f.apply(null, values);
+		   				var r = f.apply(_null, values);
 		   				if (r && isFunction(r['then']))
-		   					r['then'](function(value){newPromise(true,[value]);}, function(value){newPromise(false,[value]);});
+		   					r['then'](function(value){newPromise(_true,[value]);}, function(value){newPromise(_false,[value]);});
 		   				else
-		   					newPromise(true, [r]);
+		   					newPromise(_true, [r]);
 		   			}
 		   			else
 		   				newPromise(state, values);
 				}
 				catch (e) {
-					newPromise(false, [e]);
+					newPromise(_false, [e]);
 				}
 			};
-			if (state != null)
+			if (state != _null)
 				delay(callCallbacks);
 			else
 				deferred.push(callCallbacks);    		
@@ -931,7 +932,7 @@ define('minified', function() {
 		var self = this, r;
 		var f = isFunction(findFunc) ? findFunc : function(obj, index) { if (findFunc === obj) return index; };
 		for (var i = startIndex; i < self.length; i++)
-			if ((r = f(self[i], i)) != null)
+			if ((r = f(self[i], i)) != _null)
 				return r;
 	},
 	
@@ -1000,7 +1001,7 @@ define('minified', function() {
 			else if (nodeType < 5)        // 2 is impossible (attribute), so only 3 (text) and 4 (cdata)..
 				return e['data'];
 			else 
-				return null;
+				return _null;
 		}
 		return collector(flexiEach, this, extractString)['join']('');
 	},
@@ -1054,7 +1055,7 @@ define('minified', function() {
  	 *         have been visited when traversing another node. Duplicate nodes will be automatically removed.
  	 */
 	'trav': function(property, selector, maxDepth) {
-		var f = getFilterFunc(isNumber(selector) ? null : selector);
+		var f = getFilterFunc(isNumber(selector) ? _null : selector);
 		var max = isNumber(selector) ? selector : maxDepth || 1e9;
 		return new M(collectUniqNodes(this, function(node) {
 				var r = [];
@@ -1132,7 +1133,7 @@ define('minified', function() {
  	 */
 	'is': function(selector) {
 		var f = getFilterFunc(selector);
-		return !this['find'](function(v) {if (!f(v)) return true;});
+		return !this['find'](function(v) {if (!f(v)) return _true;});
 	},
 	
  	/*$
@@ -1270,9 +1271,9 @@ define('minified', function() {
 				// @condblock scrollxy
 				// @condblock ie8compatibility 
 				else if (spec == '$$scrollX') // for non-IE, $scrollX/Y fall right thought to element[name]...
-					s = _window['pageXOffset'] != null ? _window['pageXOffset'] : (_document['documentElement'] || _document['body']['parentNode'] || _document['body'])['scrollLeft'];
+					s = _window['pageXOffset'] != _null ? _window['pageXOffset'] : (_document['documentElement'] || _document['body']['parentNode'] || _document['body'])['scrollLeft'];
 				else if (spec == '$$scrollY')
-					s = _window['pageXOffset'] != null ? _window['pageYOffset'] : (_document['documentElement'] || _document['body']['parentNode'] || _document['body'])['scrollTop'];
+					s = _window['pageXOffset'] != _null ? _window['pageYOffset'] : (_document['documentElement'] || _document['body']['parentNode'] || _document['body'])['scrollTop'];
 				// @condend ie8compatibility
 				// @condend scrollxy
 				else if (/^\$[^$]/.test(spec)) {
@@ -1281,7 +1282,7 @@ define('minified', function() {
 						s = (element.currentStyle||element['style'])[name];
 					else 
 					// @condend
-						s = _window.getComputedStyle(element, null).getPropertyValue(replace(name, /[A-Z]/g, function (match) {  return '-' + match.toLowerCase(); }));
+						s = _window.getComputedStyle(element, _null).getPropertyValue(replace(name, /[A-Z]/g, function (match) {  return '-' + match.toLowerCase(); }));
 				}
 				else if (/^[@%]/.test(spec))
 					s = element.getAttribute(name);
@@ -1440,7 +1441,7 @@ define('minified', function() {
 	 */
      'set': function (name, value) {
     	 function setAttr(obj, n, v) {
-    		 if (v != null)  
+    		 if (v != _null)  
     			 obj.setAttribute(n, v);
 			 else
 				 obj.removeAttribute(n);
@@ -1471,7 +1472,7 @@ define('minified', function() {
     				 var newObj = /^\$/.test(name) ? obj.style : obj;
     				 var newValue = isFunction(value) ? value($(obj).get(name), c, obj) : value;
     				 if (name == '$') {
-    					 if (newValue != null) {
+    					 if (newValue != _null) {
     						 flexiEach(newValue.split(/\s+/), function(clzz) {
     							 var cName = replace(clzz, /^[+-]/);
     							 var oldClassName = className;
@@ -1606,7 +1607,7 @@ define('minified', function() {
 					flexiEach(c, appendChildren);
 				else if (isFunction(c))
 					appendChildren(c(e, index));
-				else if (c != null) {   // must check null, as 0 is a valid parameter 
+				else if (c != _null) {   // must check null, as 0 is a valid parameter 
 					var n = isNode(c) ? c : _document.createTextNode(c);
 					if (lastAdded)
 						lastAdded.parentNode.insertBefore(n, lastAdded.nextSibling);
@@ -1616,7 +1617,7 @@ define('minified', function() {
 						e.appendChild(n);
 					lastAdded = n;
 				}
-			})(isNode(children) && index ? null : children);
+			})(isNode(children) && index ? _null : children);
 		});
 	},
 
@@ -2060,7 +2061,7 @@ define('minified', function() {
 			if (nodeType == 1) {
 				var attrs = {
 						// @condblock ie8compatibility
-						'$': e['className'] || null,
+						'$': e['className'] || _null,
 						'$$': IS_PRE_IE9 ? e['style']['cssText'] : e.getAttribute('style')
 						// @condend
 				};
@@ -2081,7 +2082,7 @@ define('minified', function() {
 			else if (nodeType < 5)        // 2 is impossible (attribute), so only 3 (text) and 4 (cdata)..
 				return e['data'];
 			else 
-				return null;
+				return _null;
 		});
 	},
 
@@ -2224,7 +2225,7 @@ define('minified', function() {
 		};
 		state = state || {};
 		state['time'] = 0;
-		state['stop'] = function() { loopStop(); prom(false); };
+		state['stop'] = function() { loopStop(); prom(_false); };
 		durationMs = durationMs || 500;
 		linearity = linearity || 0;
 		
@@ -2232,8 +2233,9 @@ define('minified', function() {
 		flexiEach(self, function(li, index) {
 			var p = {o:$(li), e:{}}; 
 			eachObj(p.s = p.o.get(properties), function(name, start) {
+				var dest = properties[name];
 				p.e[name] = isFunction(dest) ? dest(start, index, li) : 
-					name == '$$slide' ? properties[name]*getNaturalHeight(p.o) + 'px' : properties[name];
+					name == '$$slide' ? properties[name]*getNaturalHeight(p.o) + 'px' : dest;
 			});
 			initState.push(p);
 		});
@@ -2253,8 +2255,8 @@ define('minified', function() {
 					isi.o.set(isi.e);
 				});
 				loopStop();
-				state['time'] = null;
-				prom(true, [self]);
+				state['time'] = _null;
+				prom(_true, [self]);
 			}
 			else
 				flexiEach(initState, function(isi) {
@@ -2355,13 +2357,13 @@ define('minified', function() {
 		'toggle': function(stateDesc1, stateDesc2, durationMs, linearity) {
 			var self = this;
 			var animState = {};
-			var state = false, regexg = /\b(?=\w)/g, stateDesc;
+			var state = _false, regexg = /\b(?=\w)/g, stateDesc;
 
 			if (stateDesc2)
 				return self['set'](stateDesc1) && 
 				    function(newState) {
 						if (newState !== state) {
-							stateDesc = (state = newState===true||newState===false ? newState : !state) ? stateDesc2 : stateDesc1;
+							stateDesc = (state = newState===_true||newState===_false ? newState : !state) ? stateDesc2 : stateDesc1;
 							
 							if (isFunction(stateDesc)) 
 								return stateDesc(self, durationMs, linearity);
@@ -2548,7 +2550,7 @@ define('minified', function() {
 			return this['each'](function(el, index) {
 				flexiEach(eventName.split(/\s/), function(namePrefixed) {
 					var name = replace(namePrefixed, /\|/);
-					var noSelector = isFunction(handlerOrSelector) || null;
+					var noSelector = isFunction(handlerOrSelector) || _null;
 					var handler = noSelector ? handlerOrSelector : fThisOrArgsOrHandler;
 
 					var miniHandler = createEventHandler(handler, 
@@ -2568,7 +2570,7 @@ define('minified', function() {
 					}
 					else {
 					// @condend
-						el.addEventListener(name, miniHandler, false); // W3C DOM
+						el.addEventListener(name, miniHandler, _false); // W3C DOM
 						push(el, MINIFIED_MAGIC_EVENTS, handlerDescriptor);
 					// @condblock ie8compatibility
 					}
@@ -2764,25 +2766,25 @@ define('minified', function() {
 			xhr = _window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Msxml2.XMLHTTP.3.0");
 			//@condend
 			// @cond !ie6compatibility xhr = new XMLHttpRequest();
-			if (data != null) {
+			if (data != _null) {
 				headers = headers || {};
 				if (!isString(data) && !isNode(data)) { // if data is parameter map...
 					body = collector(eachObj, data, function processParam(paramName, paramValue) {
 						return collector(flexiEach, paramValue, function(v) {
-							return encodeURIComponent(paramName) + ((v != null) ?  '=' + encodeURIComponent(v) : '');
+							return encodeURIComponent(paramName) + ((v != _null) ?  '=' + encodeURIComponent(v) : '');
 						});
 					}).join('&');
 				}
 				
 				if (!/post/i.test(method)) {
 					url += '?' + body;
-					body = null;
+					body = _null;
 				}
 				else if (!isNode(data) && !isString(data) && !headers[ContentType])
 					headers[ContentType] = 'application/x-www-form-urlencoded';
 			}
 			
-			xhr['open'](method, url, true, username, password);
+			xhr['open'](method, url, _true, username, password);
 			eachObj(headers, function(hdrName, hdrValue) {
 				xhr['setRequestHeader'](hdrName, hdrValue);
 			});
@@ -2790,10 +2792,10 @@ define('minified', function() {
 			xhr.onreadystatechange = function() {
 				if (xhr['readyState'] == 4 && !callbackCalled++) {
 					if (xhr['status'] == 200) {
-						prom(true, [xhr['responseText'], xhr['responseXML']]);
+						prom(_true, [xhr['responseText'], xhr['responseXML']]);
 					}
 					else
-						prom(false, [xhr['status'], xhr['statusText'], xhr['responseText']]);
+						prom(_false, [xhr['status'], xhr['statusText'], xhr['responseText']]);
 				}
 			};
 			
@@ -2801,7 +2803,7 @@ define('minified', function() {
 		}
 		catch (e) {
 			if (!callbackCalled) 
-				prom(false, [0, null, toString(e)]);
+				prom(_false, [0, _null, toString(e)]);
 		}
 		return prom;
 	},
@@ -2847,7 +2849,7 @@ define('minified', function() {
     */
     // @condblock ie7compatibility
     'toJSON': function toJSON(value) {
-		if (value == null)
+		if (value == _null)
 			return ""+value;                  //result: "null"; toString(value) is not possible, because it returns an empty string for null
 		if (isString(value = value.valueOf()))
 			return '"' + replace(value, /[\\\"\x00-\x1f\x22\x5c]/g, ucode) + '"' ;
@@ -3039,14 +3041,12 @@ define('minified', function() {
 	*   var radius = 100;
 	*   var d = 3000;                                        // duration in ms
 	*   $.loop(function(t, stopFunc) {
-	*     if (t > d) {                                       // time is up: call stopFunc()!
-	*       stopFunc();
-	*       return;
-	*     }
-	* 
-	*     var a = 2 * Math.PI * t / rotationsPerMs           // angular position
+	*     var a = 2 * Math.PI * Math.min(t, d) / rotationsPerMs; // angular position
 	*     myDiv.style.left = (radius * Math.cos(a) + ' px';
 	*     myDiv.style.top = (radius * Math.sin(a) + ' px';
+	*     
+	*     if (t > d)                                         // time is up: call stopFunc()!
+	*       stopFunc();
 	*   });
 	* </pre>
 	*
@@ -3110,13 +3110,13 @@ define('minified', function() {
 			}
 			else {
 			// @condend
-				h['e'].removeEventListener(h['n'], h['h'], false); // W3C DOM
+				h['e'].removeEventListener(h['n'], h['h'], _false); // W3C DOM
 				removeFromArray(h['e'][MINIFIED_MAGIC_EVENTS], h);
 			// @condblock ie8compatibility 
 			}
 			// @condend
 		});
-		handler['M'] = null;
+		handler['M'] = _null;
 	}
 
     
@@ -3142,7 +3142,7 @@ define('minified', function() {
 
     if (_document.addEventListener)
     // @condend
-    	_document.addEventListener("DOMContentLoaded", triggerDomReady, false);
+    	_document.addEventListener("DOMContentLoaded", triggerDomReady, _false);
 	/*$
 	 @stop
 	 */
@@ -3183,7 +3183,7 @@ define('minified', function() {
 		 * @syntax $(domreadyFunction)
          * @module WEB
 		 * Creates a new ##list#Minified list##, or register a DOMReady-handler. 
-		 * The most common usage is with a CSS-like selector. <var$()</var> will then create a list containing all elements of the current HTML
+		 * The most common usage is with a CSS-like selector. <var>$()</var> will then create a list containing all elements of the current HTML
 		 * document that fulfill the filter conditions. Alternatively you can also specify a list of objects or a single object. 
 		 * Nested lists will automatically be flattened, and nulls will automatically be removed from the resulting list.
 		 * 
