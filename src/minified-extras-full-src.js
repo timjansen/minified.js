e3253607d90f94aa17425d9290dee7ac9dd95cb6
tim <tim@tjansen.de>
2013-12-18 00:00:52 +0100

    doc fixes

--- src/minified-extras-full-src.js
+++ src/minified-extras-full-src.js
@@ -338,7 +338,7 @@ function dummy() {
 		 * });</pre>
 		 * 
 		 * @example Create click handlers for elements in a list:
-		 * <pre>$('#list').add(HTML('{{each}}<li>{{this.name}} <a class="del" href="#">Delete</a></li>{{each}}', items)
+		 * <pre>$('#list').add(HTML('{{each}}&lt;li>{{this.name}} &lt;a class="del" href="#">Delete&lt;/a>&lt;/li>{{each}}', items)
 		 *                .per('.del', function(el, index) {
 		 *                   el.on('click', deleteItemByName, [items[index].name]);
 		 *                }));</pre>
